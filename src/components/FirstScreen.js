e4ade6cfa41a16432f745485fb01ad13145a8e8f
ZeGuilhermeYung <jgryung@gmail.com>
2022-07-21 03:19:12 -0300

    CSS tela inicial

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -3,9 +3,11 @@ export default function FirstScreen() {
     <section className="first-screen">
       <img src="./assets/img/logo-zaprecall.png" alt="imagem de um raio amarelo com contornos vermelhos" />
       <h1>ZapRecall</h1>
-      <select name="decks" id="" placeholder="Escolha seu deck"></select>
+      <select name="decks" required>
+        <option value="" disabled selected hidden>Escolha seu deck</option>
+      </select>
       <input type="number" placeholder="Digite sua meta de zaps..." />
-      <div class="start-button">Iniciar Recall!</div>
+      <div class="start-button"><h2>Iniciar Recall!</h2></div>
     </section>
   );
 }
