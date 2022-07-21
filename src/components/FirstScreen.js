2c73079302662c41194f55abb5a420e2edbe0fd9
ZeGuilhermeYung <jgryung@gmail.com>
2022-07-21 02:13:00 -0300

    formatação de fontes, inserção de imagem naa tela inicial e criação dos componenetes da tela de jogo

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -1,7 +1,11 @@
 export default function FirstScreen() {
   return (
-    <section>
-
+    <section className="first-screen">
+      <img src="./assets/img/logo-zaprecall.png" alt="imagem de um raio amarelo com contornos vermelhos" />
+      <h1>ZapRecall</h1>
+      <select name="decks" id="" placeholder="Escolha seu deck"></select>
+      <input type="number" placeholder="Digite sua meta de zaps..." />
+      <div class="start-button">Iniciar Recall!</div>
     </section>
   );
 }
