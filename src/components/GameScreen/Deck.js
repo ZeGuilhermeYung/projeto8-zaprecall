c75118b9cf580d8ccee20eb6d141bdd2908751ce
ZeGuilhermeYung <jgryung@gmail.com>
2022-07-23 01:35:38 -0300

    implementação da lógica de geração de cards por estado e seu respectivo CSS

--- src/components/GameScreen/Deck.js
+++ src/components/GameScreen/Deck.js
@@ -2,47 +2,38 @@ import React from "react";
 
 function Card (props) {
 
-  const [frontClass, setFrontClass] = React.useState("front");
-  const [questionClass, setQuestionClass] = React.useState("question hidden");
-  const [answerClass, setAnswerClass] = React.useState("answer hidden");
-
-  function flipCard (cardClass) {
-    switch (cardClass) {
-      case "front":
-        setQuestionClass("question");
-        setFrontClass("front hidden");
-        break;
-      case "question":
-        setAnswerClass("answer");
-        setQuestionClass("question hidden");
-        break;
-      case "answer":
-        setFrontClass("front");
-        setAnswerClass("answer hidden");
-        break;
-    }     
-  }
-
-  return (
-    <div>
-      <div className={`card ${frontClass}`} onClick={() => flipCard(frontClass)}>
-        <h3>Pergunta {props.questionOrder}</h3>
-        <ion-icon name="play-outline"></ion-icon>
-      </div>
-      <div className={`card ${questionClass}`}>
-        <h2>{props.question}</h2>
-      </div>
-      <div className={`card ${answerClass}`}>
-        <h2>{props.answer}</h2>
-        <div>
-          <div className="wrong"><h5>Não lembrei</h5></div>
-          <div className="right"><h5>Quase não lembrei</h5></div>
-          <div className="zap"><h5>Zap!</h5></div>
+  const [cardState, setCardState] = React.useState("front");
+  console.log(cardState);
+  switch (cardState) {
+    case "front":
+      return (
+        <div className="card front" onClick={() => {setCardState("question")}}>
+          <h3>Pergunta {props.questionOrder}</h3>
+          <ion-icon name="play-outline"></ion-icon>
         </div>
-      </div>
-    </div>
     );
-}
+    case "question":
+      return (
+        <div className="card question">
+          <h2>{props.question}</h2>
+          <img src="./assets/img/turn-arrow.png" alt="virar para a resposta" onClick={() => {setCardState("answer")}} />
+        </div>
+      );
+    case "answer":
+      return (
+        <div className="card answer">
+          <h2>{props.answer}</h2>
+          <div>
+            <div className="recall wrong"><h5>Não lembrei</h5></div>
+            <div className="recall almost"><h5>Quase não lembrei</h5></div>
+            <div className="recall right"><h5>Zap!</h5></div>
+          </div>
+        </div>
+    );
+  };
+}     
+
+
 
 export default function Deck(props) {
 
@@ -52,7 +43,7 @@ export default function Deck(props) {
 
   return (
     <main>
-      {props.deck.qEa.sort(shuffle).map((pairQeA, index) => <Card key={index} questionOrder={index + 1} question={pairQeA.question} answer={pairQeA.answer} />)}
+      {props.deck.qEa.sort(shuffle).map((pairQeA, index) => <Card cardState={props.cardState}  setCardState={props.setCardState} key={index} questionOrder={index + 1} question={pairQeA.question} answer={pairQeA.answer} />)}
     </main>
   );
 }
